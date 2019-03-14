91bf18c60479039af8ae1a9d1f3369f556a7b6f3
Sarah Wachowicz <43891689+swachowicz@users.noreply.github.com>
2019-03-14 12:53:33 -0700

    Step75 Boolean Logic Operators

---
+++ Step75/Step75/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string yourAge = Console.ReadLine();
+            int numAge = Convert.ToInt32(yourAge);
+
+            Console.WriteLine("Have you ever had a DUI? Answer true or false");
+            string trueorfalseDUI = Console.ReadLine();
+            bool boolDUI = Convert.ToBoolean(trueorfalseDUI);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string yourTickets = Console.ReadLine();
+            int numTickets = Convert.ToInt32(yourTickets);
+
+            Console.WriteLine("Are you qualified for C#ar Insurance?");
+            bool qualified = (numAge > 15 && boolDUI == false && numTickets <= 3);
+            Console.Write(qualified.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
